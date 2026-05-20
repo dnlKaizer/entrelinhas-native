313f57f6570bf9196ff67c72122abe4114588165
dnlKaizer <danilokaizer@gmail.com>
2026-05-20 11:02:45 -0300

    fix: correção do import de useBooks

--- hooks/useBooks.ts
+++ hooks/useBooks.ts
@@ -1,9 +1,9 @@
-import type { Book } from "@/types/book";
+import { IBook } from "@/services/books.service";
 
 export function useBooks() {
   // aqui é um hook para listar os livros do usuário e buscar detalhes
   return {
-    books: [] as Book[],
+    books: [] as IBook[],
     isLoading: false,
   };
 }

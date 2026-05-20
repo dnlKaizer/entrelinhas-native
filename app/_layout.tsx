15a3fe3ec5b67f436582556bedd6dd7bb6221e39
dnlKaizer <danilokaizer@gmail.com>
2026-05-20 10:21:21 -0300

    feat: implementando rotas protegidas

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,10 @@
-import { Stack } from "expo-router";
+import { ProtectedLayout } from "@/components/ProtectedLayout";
 import { AuthProvider } from "@/providers/AuthProvider";
 
 export default function RootLayout() {
   return (
     <AuthProvider>
-      <Stack>
-        <Stack.Screen name="index" options={{ headerShown: false }} />
-        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-        <Stack.Screen name="(app)" options={{ headerShown: false }} />
-      </Stack>
+      <ProtectedLayout />
     </AuthProvider>
   );
 }

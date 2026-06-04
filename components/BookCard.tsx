62a4a87d99c4880a69702fcfd44fb86583c39430
dnlKaizer <danilokaizer@gmail.com>
2026-06-04 12:52:26 -0300

    feat: tornando BookCard clicável e redirecionando para a página de detalhes do livro

--- components/BookCard.tsx
+++ components/BookCard.tsx
@@ -2,12 +2,24 @@ import { useBookCard } from "@/hooks/useBookCard";
 import { IBook } from "@/types/book.types";
 import { Card, H4, Paragraph, YStack } from "tamagui";
 import { Image } from "expo-image";
+import { DB_SCHEMA } from "@/constants/database";
+import { useRouter } from "expo-router";
 
 export function BookCard({ book }: { book: IBook }) {
     const { title, author, imgUri } = useBookCard(book);
+    const router = useRouter();
 
     return (
-        <Card elevate size="$4" bordered animation="lazy" width={160} hoverStyle={{ scale: 1.02 }}>
+        <Card 
+            elevate 
+            size="$4" 
+            bordered 
+            animation="bouncy" 
+            width={160} 
+            hoverStyle={{ scale: 1.02 }}
+            cursor="pointer"
+            onPress={() => router.push(`/books/${book[DB_SCHEMA.BOOKS.COLUMNS.ID]}`)}
+        >
             <YStack width="100%" padding="$2" alignItems="center">
                 <Image
                     source={imgUri}

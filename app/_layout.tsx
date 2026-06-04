c13d40d2322261a9c31838553dbc91f6c9201e99
dnlKaizer <danilokaizer@gmail.com>
2026-06-04 13:06:35 -0300

    feat: adicionando fonte Lobster ao projeto

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,16 @@
 import { ProtectedLayout } from "@/components/ProtectedLayout";
 import { AuthProvider } from "@/providers/AuthProvider";
 import { ThemeProvider } from "@/providers/ThemeProvider";
+import { useFonts } from "expo-font";
 
 import "../styles.css";
+import lobsterFont from "../assets/fonts/Lobster-Regular.ttf";
 
 export default function RootLayout() {
+  useFonts({
+    lobster: lobsterFont,
+  });
+
   return (
     <ThemeProvider>
       <AuthProvider>

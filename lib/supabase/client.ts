c7456429ddf9142cc00f5e8d3329e987ddd5c51c
dnlKaizer <danilokaizer@gmail.com>
2026-06-08 21:56:46 -0300

    fix: corrigindo cliente do supabase para usar o adapter

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -1,5 +1,6 @@
 import { Database } from '@/types/database.types'
 import { createClient } from '@supabase/supabase-js'
+import { expoSecureStoreAdapter } from './adapter'
 
 const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL
 const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY
@@ -8,4 +9,11 @@ if (!supabaseUrl || !supabaseAnonKey) {
     throw new Error('Variáveis EXPO_PUBLIC_SUPABASE_URL e EXPO_PUBLIC_SUPABASE_ANON_KEY não configuradas.')
 }
 
-export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey, {
+    auth: {
+        storage: expoSecureStoreAdapter,
+        autoRefreshToken: true,
+        persistSession: true,
+        detectSessionInUrl: false,
+    },
+})
